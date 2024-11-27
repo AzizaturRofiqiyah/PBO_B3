4bd2649319fdb8c597a3539eda9e457ed25976bb
AzizaturRofiqiyah <azizaturrofiqiyah@gmail.com>
2024-11-27 17:27:56 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,75 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System;
-
-public interface ILaukPauk
+namespace winform_mvc
 {
-    int ID_Produk { get; }
-    string NamaProduk { get; set; }
-    string Deskripsi { get; set; }
-    int Harga { get; set; }
-    int StokProduk { get; set; }
-
-    void TampilkanInformasiProduk();
-    void PerbaruiStok(int jumlah);
-    int Transaksi(int jumlah);
-}
-
-public class Daging : ILaukPauk
-{
-    public int ID_Produk { get; private set; }
-    public string NamaProduk { get; set; }
-    public string Deskripsi { get; set; }
-    public int Harga { get; set; }
-    public int StokProduk { get; set; }
-    private string Jenis { get; } = "Daging";
-
-    public Daging(int idProduk, string namaProduk, string deskripsi, int harga, int stokProduk)
+    internal static class Program
     {
-        ID_Produk = idProduk;
-        NamaProduk = namaProduk;
-        Deskripsi = deskripsi;
-        Harga = harga;
-        StokProduk = stokProduk;
-    }
-    public void TampilkanInformasiProduk()
-    {
-        Console.WriteLine($"ID Produk:{ID_Produk}");
-        Console.WriteLine($"Nama Produk:{NamaProduk}");
-        Console.WriteLine($"Deskripsi: {Deskripsi}");
-        Console.WriteLine($"Harga: {Harga}");
-        Console.WriteLine($"Stok: {StokProduk}");
-        Console.WriteLine($"Jenis: {Jenis}");
-        Console.WriteLine("-------------------------------");
-
-    }
-
-    public void PerbaruiStok(int jumlah)
-    {
-        StokProduk = Math.Max(StokProduk + jumlah, 0);
-        Console.WriteLine($"Stok diperbarui, stok saat ini:{StokProduk}");
-    }
-
-    public int Transaksi(int jumlah)
-    {
-        if (jumlah > 0 && jumlah <= StokProduk)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            int totalHarga = jumlah * Harga;
-            StokProduk -= jumlah;
-            Console.WriteLine($"Transaksi berhasil, dengan total harga: Rp{totalHarga}");
-            Console.WriteLine($"Sisa stok : {StokProduk}");
-            return totalHarga;
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
         }
-        Console.WriteLine("Jumlah stok tidak mencukupi");
-        return 0;
-
     }
-
-}
-
-
-public class Tahu : ILaukPauk
-{
-
 }
---
+++ katalog.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace winform_mvc.Views
+{
+    partial class katalog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(katalog));
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1142, 450);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1069, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(52, 51);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(332, 384);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(77, 384);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Location = new Point(349, 344);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 6;
+            button2.Text = "edit harga";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(87, 344);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 5;
+            button1.Text = "edit stok";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(573, 225);
+            label2.TabIndex = 2;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Location = new Point(77, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 25);
+            label1.TabIndex = 1;
+            label1.Text = "SAYUR BAYAM FRESH";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(609, 52);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(485, 378);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // katalog
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1142, 450);
+            Controls.Add(panel1);
+            Name = "katalog";
+            Text = "katalog";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox2;
+    }
+}
